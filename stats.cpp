09050d5ac7536bce90bf9e83b88ffc7d28279be5
AnagaDankh <122591543+AnagaDankh@users.noreply.github.com>
2023-07-20 17:50:14 +0530

    Update stats.cpp
    Only Avg calculation is done.

--- stats.cpp
+++ stats.cpp
@@ -1,8 +1,19 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
+   static struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
+
+    int sum = 0;
+    int i = 0;
+
+    for(i=0;i<setlength;i++)
+    {
+        sum = sum+numberset[i];
+    }
+
+    s.average = sum/setlength;
+    return s;
 }
