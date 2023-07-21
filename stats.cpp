4e3aae7fca8d479fd5dd04f7966902347a5ee3d6
AnagaDankh <122591543+AnagaDankh@users.noreply.github.com>
2023-07-21 15:12:44 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,19 +1,71 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-   static struct Stats s;
+Stats compute_statistics(const float* numberset, int setlength) {
+    Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
-
-    int sum = 0;
     int i = 0;
+    int sum = 0;
 
-    for(i=0;i<setlength;i++)
+    if(0 == setlength%2)
     {
-        sum = sum+numberset[i];
+            if(numberset[0] > numberset[1])
+            {
+                s.max = numberset[0];
+                s.min = numberset[1];
+            }
+            else
+            {
+                s.max = numberset[1];
+                s.min = numberset[0];
+            }
+    }
+    else
+    {
+        s.max = numberset[0];
+        s.min = numberset[0];
+        i=1;
     }
 
+    while( i < (setlength-1))
+    {
+        if(numberset[i] > numberset[i+1])
+        {
+                if(numberset[i] > s.max)
+                {
+                   s.max = numberset[i];
+                }
+                if(numberset[i+1] < s.min)
+                {
+                   s.min = numberset[i+1];
+                }
+        }
+        else
+        {
+                if(numberset[i+1] > s.max)
+                {
+                   s.max = numberset[i+1];
+                }
+                if(numberset[i] < s.min)
+                {
+                  s.min = numberset[i];
+                }
+        }
+        i+=2;
+    }
+
+    for(i=0; i<setlength; i++)
+    {
+            sum = numberset[i];
+
+    }
     s.average = sum/setlength;
     return s;
+}
+
+int main()
+{
+
+    return 0;
 }
