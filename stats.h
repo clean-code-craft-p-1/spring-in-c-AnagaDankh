9eed7e45c6ac408782ed08a1507e1f7e8ec0638f
AnagaDankh <122591543+AnagaDankh@users.noreply.github.com>
2023-07-24 15:38:07 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,5 @@
 
-// define the Stats structure here. See the tests to infer its properties
+#include <stdio.h>
 // define the Stats structure here. See the tests to infer its properties
 typedef struct
 {
@@ -10,5 +10,23 @@ typedef struct
 
 Stats compute_statistics(const float* numberset, int setlength);
 
+// If the email Alert has been sent
+typedef struct
+{
+        bool emailAlertSent;
+}EmailAlertSent;
+extern EmailAlertSent Emailsent;
+
+// If the led has turned on
+typedef struct
+{
+        bool ledAlertOn;
+}LedAlertSent;
+extern LedAlertSent LedOn;
+
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
+void check_and_alert(float maxThreshold, void (*alerter_funcptr[])(), Stats computedStats);
+//Function for LedAlerter
+void emailAlerter(void);
+//Function for Email Alert sent
+void ledAlerter(void);
